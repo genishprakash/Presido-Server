e849af86672692881c30b07f2a85b6c500f023a0
Genish Prakash <genishprakash@gmail.com>
2024-05-26 21:21:46 +0530

    Reafctoring

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 const dotenv=require("dotenv").config()
 const connectDb= async ()=>{
     try{
-        const connect=await mongoose.connect(process.env.CONNECTION_STRING)
+        const connect=await mongoose.connect(`mongodb+srv://genishprakash:${process.env.MONGO_PASSWORD}@testcluster.vw17fml.mongodb.net/?retryWrites=true&w=majority&appName=testCluster`)
         console.log("Database connected")
         console.log(connect.connection.host)
         console.log(connect.connection.name)
